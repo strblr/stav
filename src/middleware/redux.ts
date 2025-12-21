235eb6aee0a419ac8518d38ede9fada8ba52cb18
strblr <30187797+strblr@users.noreply.github.com>
2025-12-21 02:39:24 +0100

    feat: redux middleware

---
+++ src/middleware/redux.ts
@@ -0,0 +1,19 @@
+import type { Store, State } from "../create";
+import { assign } from "../utils.js";
+
+export interface ReduxStore<A> {
+  dispatch: (action: A) => void;
+}
+
+export interface Reducer<T, A> {
+  (state: T, action: A): T;
+}
+
+export function redux<S extends Store<any>, A extends { type: string }>(
+  store: S,
+  reducer: Reducer<State<S>, A>
+) {
+  return assign<S, ReduxStore<A>>(store, {
+    dispatch: action => store.set(() => reducer(store.get(), action))
+  });
+}
